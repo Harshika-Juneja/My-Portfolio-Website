d59a2706a53ef68b04f2adaad1b04612b6ff59ef
Harshika-Juneja <86465532+Harshika-Juneja@users.noreply.github.com>
2024-12-18 13:19:09 +0530

    Update CTA.js

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
+import Resume from '../../assets/resume.pdf'
 import Degree from '../../assets/degree.pdf'
 const CTA = () => {
   return (
     <div>
       <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={Resume} download className='btn'>Download CV</a>
         <a href={Degree} className='btn btn-primary' target='__blank'>Download Degree</a>
       </div>
     </div>
